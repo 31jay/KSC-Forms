874e4db2bfcc800db03cadb0b93bc2fab4f087f7
31jay <102200770+31jay@users.noreply.github.com>
2025-08-12 13:48:31 +0545

    sds

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class Config:
         )
         
         if is_streamlit_cloud:
-            self.REDIRECT_URI = 'https://ksc-at-khec.streamlit.app/'
+            self.REDIRECT_URI = 'https://ksc-khec.streamlit.app/'
         else:
             self.REDIRECT_URI = 'http://localhost:8501/'
         
@@ -313,7 +313,7 @@ def show_login_page():
             </div>
         """, unsafe_allow_html=True)
 
-        
+
 def main():
     st.set_page_config(
         page_title="Google OAuth Demo",
